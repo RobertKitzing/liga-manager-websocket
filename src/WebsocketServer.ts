0646942d235138746307324875960637bf72f198
Robert Kitzing <r.kitzing@hotmail.de>
2018-04-06 13:45:38 +0200

    ws init

--- src/WebsocketServer.ts
+++ src/WebsocketServer.ts
@@ -1,22 +1,32 @@
-import * as express from 'express'
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
 class WebsocketServer {
-  public express
+  
+  public express: any;
+  public server: any;
+  public websocket: any;
 
   constructor () {
-    this.express = express()
-    this.mountRoutes()
-  }
+    this.express = express();
+    const server = http.createServer(this.express);
+    this.server = server;
+    this.websocket = new WebSocket.Server({server});
+    this.websocket.on('connection', (ws: WebSocket) => {
 
-  private mountRoutes (): void {
-    const router = express.Router()
-    router.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!'
-      })
-    })
-    this.express.use('/', router)
+      //connection is up, let's add a simple simple event
+      ws.on('message', (message: string) => {
+  
+          //log the received message and send it back to the client
+          console.log('received: %s', message);
+          ws.send(`Hello, you sent -> ${message}`);
+      });
+  
+      //send immediatly a feedback to the incoming connection    
+      ws.send('Hi there, I am a WebSocket server');
+  });
   }
 }
 
-export default new WebsocketServer().express
+export default new WebsocketServer().server;
